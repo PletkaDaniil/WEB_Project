279d1bc8e08fb821de5b84261e2ab5cc057579a7
Pletka Daniil <pletkadaniil@yandex.ru>
2024-12-06 22:44:38 +0300

    google auth and fix login page

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,30 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session, abort
 from .models import User
 from . import db
-from flask_login import login_user, login_required, logout_user, current_user
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import re
+import os
+import pathlib
+from google_auth_oauthlib.flow import Flow
+from google.oauth2 import id_token
+import requests
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
 
 auth = Blueprint("auth", __name__)
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+# Настройки для Google OAuth
+GOOGLE_CLIENT_ID = "325915715071-aiemmsuu0pm46jeloal67d1ncu83sh97.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
 
 def is_email_valid(email):
     email_regex = r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$'
@@ -29,6 +48,56 @@ def login():
             flash("Email doesn't exist.", category="error")
 
     return render_template("login.html", user=current_user)
+
+@auth.route("/login/google")
+def login_google():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+@auth.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID,
+        clock_skew_in_seconds=10
+    )
+
+    email = id_info.get("email")
+    name = id_info.get("name")
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        new_user = User(
+            email=email,
+            username=name,
+            password=generate_password_hash(email, method='pbkdf2:sha256')
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+    else:
+        login_user(user, remember=True)
+
+    return redirect(url_for("views.home"))
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("views.home"))
+
 
 @auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
@@ -62,9 +131,3 @@ def sign_up():
             return redirect(url_for("views.home"))
 
     return render_template("sign_up.html", user=current_user)
-
-@auth.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("views.home"))
